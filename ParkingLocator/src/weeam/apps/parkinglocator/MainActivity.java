68eb17bed9c42912e2148131dacb3498d4766d20
Weeam93 <weeam93@gmail.com>
2014-05-19 20:48:32 -0700

    Added new Activities

--- ParkingLocator/src/weeam/apps/parkinglocator/MainActivity.java
+++ ParkingLocator/src/weeam/apps/parkinglocator/MainActivity.java
@@ -1,6 +1,5 @@
 package weeam.apps.parkinglocator;
 
-
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
---
+++ ParkingLocator/src/weeam/apps/parkinglocator/MapPage.java
@@ -0,0 +1,15 @@
+package weeam.apps.parkinglocator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MapPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_page);
+	}
+}
